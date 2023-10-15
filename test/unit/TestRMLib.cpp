d45b1efa89af9221d8d2b2ec7d712c6fe312f2f5
Timothy Werquin <timothy@werquin.com>
2023-10-15 15:55:17 +0200

    rMlib: More unit tests

--- test/unit/TestRMLib.cpp
+++ test/unit/TestRMLib.cpp
@@ -1,18 +1,25 @@
-
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
+
+#include "rMLibTestHelper.h"
 
 #include <FrameBuffer.h>
 #include <Input.h>
 
 #include <UI/AppContext.h>
+#include <UI/Button.h>
+#include <UI/DynamicWidget.h>
+#include <UI/Flex.h>
 #include <UI/Layout.h>
+#include <UI/Navigator.h>
+#include <UI/StatelessWidget.h>
 #include <UI/Text.h>
 
 #include <SDL_events.h>
 
-using namespace rmlib;
+#include <filesystem>
 
-constexpr auto default_pump = std::chrono::milliseconds(1);
+using namespace rmlib;
 
 extern bool rmlib_disable_window;
 
@@ -20,83 +27,6 @@ const auto disable_window_on_start = [] {
   rmlib_disable_window = true;
   return true;
 }();
-
-using Finder = std::function<bool(const RenderObject*)>;
-
-struct TestContext : AppContext {
-  static TestContext make() {
-    auto appCtx = AppContext::makeContext();
-    REQUIRE(appCtx.has_value());
-    return TestContext(std::move(*appCtx));
-  }
-
-  TestContext(AppContext appCtx) : AppContext(std::move(appCtx)) {}
-
-  template<typename Widget>
-  void pumpWidget(const Widget& widget) {
-    setRootRenderObject(widget.createRenderObject());
-    pump();
-  }
-
-  void pump(std::chrono::milliseconds duration = default_pump) {
-    bool running = true;
-    auto handle = addTimer(duration, [&running] { running = false; });
-    while (running) {
-      step();
-    }
-  }
-
-  std::vector<RenderObject*> getAllNodes(RenderObject* obj = nullptr) {
-    if (obj == nullptr) {
-      obj = &getRootRenderObject();
-    }
-
-    std::vector<RenderObject*> result;
-    result.emplace_back(obj);
-
-    for (auto* child : obj->getChildren()) {
-      auto childNodes = getAllNodes(child);
-      result.insert(result.end(), childNodes.begin(), childNodes.end());
-    }
-    return result;
-  }
-
-  // TODO: find stuff
-  template<typename Widget>
-  std::vector<typename Widget::RenderObjectType*> findByType() {
-    auto res =
-      find([typeID = rmlib::typeID::type_id<Widget>()](const auto& ro) {
-        return ro->getWidgetTypeID() == typeID;
-      });
-
-    std::vector<typename Widget::RenderObjectType*> casted;
-    casted.reserve(res.size());
-    std::transform(
-      res.begin(), res.end(), std::back_inserter(casted), [](auto* ro) {
-        return static_cast<typename Widget::RenderObjectType*>(ro);
-      });
-
-    return casted;
-  }
-
-  std::vector<TextRenderObject*> findByText(std::string_view text) {
-    auto res = findByType<Text>();
-    res.erase(std::remove_if(
-                res.begin(),
-                res.end(),
-                [text](auto* ro) { return ro->getWidget().getText() != text; }),
-              res.end());
-    return res;
-  }
-
-  std::vector<RenderObject*> find(Finder finder) {
-    auto nodes = getAllNodes();
-    nodes.erase(std::remove_if(nodes.begin(), nodes.end(), std::not_fn(finder)),
-                nodes.end());
-
-    return nodes;
-  }
-};
 
 TEST_CASE("Init Framebuffer", "[rmlib]") {
   auto fb = rmlib::fb::FrameBuffer::open();
@@ -136,4 +66,292 @@ TEST_CASE("Text", "[rmlib][ui]") {
 
   auto txt2 = ctx.findByText("Test");
   REQUIRE(txt == txt2);
+
+  REQUIRE_THAT(ctx.findByText("Test"), ctx.matchesGolden("text-basic.png"));
 }
+
+TEST_CASE("Sized", "[rmlib][ui]") {
+  auto ctx = TestContext::make();
+
+  SECTION("limited") {
+    ctx.pumpWidget(Center(Sized(Text("Test"), 50, 50)));
+
+    auto txt = ctx.findByText("Test");
+    REQUIRE_THAT(txt, Catch::Matchers::SizeIs(1));
+
+    REQUIRE(txt.front()->getSize() == Size{ 50, 50 });
+    REQUIRE_THAT(txt, ctx.matchesGolden("text-sized.png"));
+  }
+
+  SECTION("unlimited") {
+    ctx.pumpWidget(Center(Sized(Text("Test"), std::nullopt, std::nullopt)));
+
+    auto txt = ctx.findByText("Test");
+    REQUIRE_THAT(txt, Catch::Matchers::SizeIs(1));
+
+    REQUIRE(txt.front()->getSize() != Size{ 50, 50 });
+    REQUIRE_THAT(txt, ctx.matchesGolden("text-sized2.png"));
+  }
+
+  SECTION("partial") {
+    ctx.pumpWidget(Center(Sized(Text("Test"), std::nullopt, 20)));
+
+    auto txt = ctx.findByText("Test");
+    REQUIRE_THAT(txt, Catch::Matchers::SizeIs(1));
+
+    REQUIRE(txt.front()->getSize().height == 20);
+    REQUIRE_THAT(txt, ctx.matchesGolden("text-sized3.png"));
+  }
+
+  SECTION("partial") {
+    ctx.pumpWidget(Center(Sized(Text("Test"), 200, std::nullopt)));
+
+    auto txt = ctx.findByText("Test");
+    REQUIRE_THAT(txt, Catch::Matchers::SizeIs(1));
+
+    REQUIRE(txt.front()->getSize().width == 200);
+    REQUIRE_THAT(txt, ctx.matchesGolden("text-sized4.png"));
+  }
+}
+
+TEST_CASE("Container", "[rmlib][ui]") {
+  auto ctx = TestContext::make();
+  ctx.pumpWidget(Center(
+    Container(Text("Test"), Insets::all(5), Insets::all(6), Insets::all(3))));
+  REQUIRE_THAT(ctx.findByType<Padding<Border<Padding<Text>>>>(),
+               ctx.matchesGolden("container-text.png"));
+}
+
+TEST_CASE("Flex", "[rmlib][ui]") {
+  auto ctx = TestContext::make();
+
+  SECTION("Column") {
+    ctx.pumpWidget(Center(Column(Text("Foo"), Text("Bar"))));
+
+    auto column = ctx.findByType<Flex<Text, Text>>();
+    REQUIRE_THAT(column, ctx.matchesGolden("column-text.png"));
+  }
+
+  SECTION("Row") {
+    ctx.pumpWidget(Center(Row(Text("Foo"), Text("Bar"))));
+
+    auto column = ctx.findByType<Flex<Text, Text>>();
+    REQUIRE_THAT(column, ctx.matchesGolden("row-text.png"));
+  }
+
+  SECTION("Expanded") {
+    ctx.pumpWidget(
+      Center(Row(Text("Foo"), Expanded(Text("Test")), Text("Bar"))));
+
+    auto column = ctx.findByType<Flex<Text, Expanded<Text>, Text>>();
+    REQUIRE_THAT(column, ctx.matchesGolden("expanded-text.png"));
+  }
+
+  SECTION("Expanded - flex") {
+    ctx.pumpWidget(
+      Center(Sized(Column(Expanded(Border(Text("Foo"), Insets::all(2)), 2.0f),
+                          Expanded(Border(Text("Test"), Insets::all(2))),
+                          Border(Text("Bar"), Insets::all(2))),
+                   200,
+                   364)));
+
+    auto txt1 = ctx.findByText("Foo");
+    auto txt2 = ctx.findByText("Test");
+    auto txt3 = ctx.findByText("Bar");
+    REQUIRE(txt1.size() == 1);
+    REQUIRE(txt2.size() == 1);
+    REQUIRE(txt3.size() == 1);
+
+    REQUIRE(txt1.front()->getSize().height ==
+            Catch::Approx(txt2.front()->getSize().height * 2).margin(10));
+
+    auto column = ctx.findByType<
+      Flex<Expanded<Border<Text>>, Expanded<Border<Text>>, Border<Text>>>();
+    REQUIRE_THAT(column, ctx.matchesGolden("expanded-flex-text.png"));
+  }
+}
+
+class LabledInt : public StatelessWidget<LabledInt> {
+public:
+  LabledInt(std::string label, int n) : label(std::move(label)), integer(n) {}
+
+  auto build(AppContext& context, const BuildContext&) const {
+    return Row(Text(label), Text(std::to_string(integer)));
+  }
+
+private:
+  std::string label;
+  int integer;
+};
+
+TEST_CASE("Stateless", "[rmlib][ui]") {
+  auto ctx = TestContext::make();
+  ctx.pumpWidget(Center(LabledInt("Foo: ", 55)));
+
+  REQUIRE_THAT(ctx.findByType<LabledInt>(), ctx.matchesGolden("stateless.png"));
+}
+
+TEST_CASE("Button", "[rmlib][ui]") {
+  auto ctx = TestContext::make();
+
+  int clicked = 0;
+
+  ctx.pumpWidget(Center(Button("Click", [&] { clicked++; })));
+  REQUIRE(clicked == 0);
+
+  auto btnTxt = ctx.findByText("Click");
+  auto btn = ctx.findByType<Button>();
+
+  REQUIRE_THAT(btn, ctx.matchesGolden("button-released.png"));
+  ctx.press(btnTxt);
+  ctx.pump();
+  REQUIRE(clicked == 0);
+  REQUIRE_THAT(btn, ctx.matchesGolden("button-pressed.png"));
+  ctx.release(btnTxt);
+  ctx.pump();
+  REQUIRE(clicked == 1);
+  REQUIRE_THAT(btn, ctx.matchesGolden("button-released.png"));
+
+  ctx.tap(btnTxt);
+  REQUIRE(clicked == 2);
+}
+
+class CounterTest : public StatefulWidget<CounterTest> {
+public:
+  class State : public StateBase<CounterTest> {
+  public:
+    void init(AppContext& context, const BuildContext&) {}
+
+    DynamicWidget build(AppContext& context, const BuildContext&) const {
+      if (count < 5) {
+        return Column(LabledInt("Counter: ", count),
+                      Row(Button("-1", [this] { decrease(); }),
+                          Button("+1", [this] { increase(); })));
+      } else {
+        return Button("reset", [this]() { reset(); });
+      }
+    }
+
+  private:
+    void reset() const {
+      setState([](auto& self) { self.count = 0; });
+    }
+    void increase() const {
+      setState([](auto& self) { self.count++; });
+    }
+
+    void decrease() const {
+      setState([](auto& self) { self.count--; });
+    }
+
+    int count = 0;
+    TimerHandle timer;
+  };
+
+public:
+  State createState() const { return State{}; }
+};
+
+TEST_CASE("StateFull - Dynamic", "[rmlib][ui]") {
+  auto ctx = TestContext::make();
+  ctx.pumpWidget(Center(CounterTest()));
+
+  auto counter = ctx.findByType<CounterTest>();
+
+  {
+    auto countTxt = ctx.findByText("0");
+    auto incTxt = ctx.findByText("+1");
+    auto decTxt = ctx.findByText("-1");
+
+    REQUIRE_THAT(countTxt, Catch::Matchers::SizeIs(1));
+    REQUIRE_THAT(incTxt, Catch::Matchers::SizeIs(1));
+    REQUIRE_THAT(decTxt, Catch::Matchers::SizeIs(1));
+
+    REQUIRE_THAT(counter, ctx.matchesGolden("counter-init.png"));
+
+    ctx.tap(incTxt);
+    ctx.pump();
+    REQUIRE(countTxt.front()->getWidget().getText() == "1");
+    REQUIRE_THAT(counter, ctx.matchesGolden("counter-1.png"));
+
+    ctx.tap(decTxt);
+    ctx.pump();
+    REQUIRE(countTxt.front()->getWidget().getText() == "0");
+    REQUIRE_THAT(counter, ctx.matchesGolden("counter-init.png"));
+
+    for (int i = 1; i < 5; i++) {
+      ctx.tap(incTxt);
+      ctx.pump();
+      REQUIRE(countTxt.front()->getWidget().getText() == std::to_string(i));
+    }
+    REQUIRE_THAT(counter, ctx.matchesGolden("counter-5.png"));
+
+    ctx.tap(incTxt);
+    ctx.pump();
+    REQUIRE_THAT(counter, ctx.matchesGolden("counter-reset.png"));
+  }
+
+  auto resetBtn = ctx.findByText("reset");
+  ctx.tap(resetBtn);
+  ctx.pump();
+  REQUIRE_THAT(counter, ctx.matchesGolden("counter-init.png"));
+}
+
+class DialogTest : public StatelessWidget<DialogTest> {
+public:
+  DialogTest(std::string msg) : msg(std::move(msg)) {}
+
+  auto build(AppContext&, const BuildContext& ctx) const {
+    return Border(
+      Cleared(Column(
+        Text(msg),
+        Row(Padding(Button("OK", [&ctx] { Navigator::of(ctx).pop(true); }),
+                    Insets::all(10)),
+            Padding(Button("Cancel", [&ctx] { Navigator::of(ctx).pop(false); }),
+                    Insets::all(10))))),
+      Insets::all(5));
+  }
+
+  std::string msg;
+};
+
+class NavTest : public StatelessWidget<NavTest> {
+public:
+  auto build(AppContext&, const BuildContext& buildCtx) const {
+    return Button("show dialog", [&buildCtx] {
+      Navigator::of(buildCtx)
+        .push<bool>([] { return DialogTest("First dialog"); })
+        .then([&buildCtx](bool val) {
+          std::cout << "Closed First\n";
+          return Navigator::of(buildCtx).push<bool>([val] {
+            return DialogTest("Second dialog: " +
+                              std::string(val ? "OK" : "Cancel"));
+          });
+        })
+        .then([](bool val) { std::cout << "Closed second\n"; });
+    });
+  }
+};
+
+TEST_CASE("Navigator", "[rmlib][ui]") {
+  auto ctx = TestContext::make();
+
+  ctx.pumpWidget(Center(Navigator(NavTest())));
+  auto navTest = ctx.findByType<Navigator>();
+
+  REQUIRE_THAT(navTest, ctx.matchesGolden("nav-init.png"));
+
+  ctx.tap(ctx.findByText("show dialog"));
+  ctx.pump();
+  REQUIRE_THAT(navTest, ctx.matchesGolden("nav-second.png"));
+
+  ctx.tap(ctx.findByText("OK"));
+  ctx.pump();
+  REQUIRE_THAT(navTest, ctx.matchesGolden("nav-third.png"));
+
+  ctx.tap(ctx.findByText("OK"));
+  ctx.pump();
+  REQUIRE_THAT(navTest, ctx.matchesGolden("nav-init.png"));
+}
+
+// TODO: image, colored, Positioned
---
+++ test/unit/rMLibTestHelper.h
@@ -0,0 +1,179 @@
+#pragma once
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_templated.hpp>
+
+#include <UI/AppContext.h>
+#include <UI/Text.h>
+
+#include <filesystem>
+
+#include <SDL_events.h>
+
+const std::filesystem::path assets_path = ASSETS_PATH;
+
+using Finder = std::function<bool(const rmlib::RenderObject*)>;
+
+template<typename RO = rmlib::RenderObject>
+using FindResult = std::vector<RO*>;
+
+struct TestContext : rmlib::AppContext {
+  struct GoldenImageMatcher : Catch::Matchers::MatcherGenericBase {
+    static inline const bool update_golden_files = [] {
+      auto* update = std::getenv("UPDATE_GOLDEN");
+      return update != nullptr && std::string_view(update) == "1";
+    }();
+
+    GoldenImageMatcher(std::filesystem::path path, TestContext& ctx)
+      : path(path), ctx(ctx) {}
+
+    template<typename RO>
+    bool match(const FindResult<RO>& other) const {
+      if (other.size() != 1) {
+        return false;
+      }
+
+      if (update_golden_files) {
+        ctx.writeImage(path, *other.front());
+        return true;
+      }
+
+      const auto canvas =
+        ctx.framebuffer.canvas.subCanvas(other.front()->getRect());
+      const auto goldenCanvas = rmlib::ImageCanvas::load(path.c_str());
+      if (!goldenCanvas.has_value()) {
+        return false;
+      }
+
+      return canvas == goldenCanvas->canvas;
+    }
+
+    std::string describe() const override { return "Matches image"; }
+
+    std::filesystem::path path;
+    TestContext& ctx;
+  };
+
+  static constexpr auto default_pump = std::chrono::milliseconds(1);
+
+  static TestContext make() {
+    auto appCtx = AppContext::makeContext();
+    REQUIRE(appCtx.has_value());
+    return TestContext(std::move(*appCtx));
+  }
+
+  TestContext(AppContext appCtx) : AppContext(std::move(appCtx)) {
+    framebuffer.clear();
+  }
+
+  template<typename Widget>
+  void pumpWidget(const Widget& widget) {
+    setRootRenderObject(widget.createRenderObject());
+    pump();
+  }
+
+  void pump(std::chrono::milliseconds duration = default_pump) {
+    bool running = true;
+    auto handle = addTimer(duration, [&running] { running = false; });
+    while (running) {
+      step();
+    }
+  }
+
+  FindResult<> getAllNodes(rmlib::RenderObject* obj = nullptr) {
+    if (obj == nullptr) {
+      obj = &getRootRenderObject();
+    }
+
+    std::vector<rmlib::RenderObject*> result;
+    result.emplace_back(obj);
+
+    for (auto* child : obj->getChildren()) {
+      auto childNodes = getAllNodes(child);
+      result.insert(result.end(), childNodes.begin(), childNodes.end());
+    }
+    return result;
+  }
+
+  // TODO: find stuff
+  template<typename Widget>
+  FindResult<typename Widget::RenderObjectType> findByType() {
+    auto res =
+      find([typeID = rmlib::typeID::type_id<Widget>()](const auto& ro) {
+        return ro->getWidgetTypeID() == typeID;
+      });
+
+    std::vector<typename Widget::RenderObjectType*> casted;
+    casted.reserve(res.size());
+    std::transform(
+      res.begin(), res.end(), std::back_inserter(casted), [](auto* ro) {
+        return static_cast<typename Widget::RenderObjectType*>(ro);
+      });
+
+    return casted;
+  }
+
+  FindResult<rmlib::TextRenderObject> findByText(std::string_view text) {
+    auto res = findByType<rmlib::Text>();
+    res.erase(std::remove_if(
+                res.begin(),
+                res.end(),
+                [text](auto* ro) { return ro->getWidget().getText() != text; }),
+              res.end());
+    return res;
+  }
+
+  std::vector<rmlib::RenderObject*> find(Finder finder) {
+    auto nodes = getAllNodes();
+    nodes.erase(std::remove_if(nodes.begin(), nodes.end(), std::not_fn(finder)),
+                nodes.end());
+
+    return nodes;
+  }
+
+  void writeImage(std::filesystem::path path, const rmlib::RenderObject& ro) {
+    const auto canvas = framebuffer.canvas.subCanvas(ro.getRect());
+    REQUIRE(rmlib::writeImage(path.c_str(), canvas).has_value());
+  }
+
+  GoldenImageMatcher matchesGolden(std::string_view name) {
+    return GoldenImageMatcher(assets_path / name, *this);
+  }
+
+  void sendEv(SDL_Event& ev) {
+    SDL_PushEvent(&ev);
+    step();
+  }
+
+  template<typename RO>
+  void press(const FindResult<RO>& ros) {
+    REQUIRE(ros.size() == 1);
+    auto pos = ros.front()->getRect().center();
+
+    SDL_Event ev;
+    ev.type = SDL_MOUSEBUTTONDOWN;
+    ev.motion.x = pos.x / EMULATE_SCALE;
+    ev.motion.y = pos.y / EMULATE_SCALE;
+    ev.button.button = SDL_BUTTON_LEFT;
+    sendEv(ev);
+  }
+
+  template<typename RO>
+  void release(const FindResult<RO>& ros) {
+    REQUIRE(ros.size() == 1);
+    auto pos = ros.front()->getRect().center();
+
+    SDL_Event ev;
+    ev.type = SDL_MOUSEBUTTONUP;
+    ev.motion.x = pos.x / EMULATE_SCALE;
+    ev.motion.y = pos.y / EMULATE_SCALE;
+    ev.button.button = SDL_BUTTON_LEFT;
+    sendEv(ev);
+  }
+
+  template<typename RO>
+  void tap(const FindResult<RO>& ros) {
+    press(ros);
+    release(ros);
+  }
+};
