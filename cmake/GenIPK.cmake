ba361cd0acf7254dfc5ffb8ac2c5898d341aee53
Timothy Werquin <timothy@werquin.com>
2023-10-26 18:11:46 +0200

    Add cmake component based install
    Produces a package per sub-project.

--- cmake/GenIPK.cmake
+++ cmake/GenIPK.cmake
@@ -1,17 +1,21 @@
 # Cmake generates ar archives instead of tar.gz.
 # So this post archive script re-packages the .deb using tar.
 
-get_filename_component(PARENT_DIR "${CPACK_PACKAGE_FILES}" DIRECTORY)
-set(TEMP_DIR "${PARENT_DIR}/tmp")
-file(MAKE_DIRECTORY "${TEMP_DIR}")
+foreach(PACKAGE_FILE IN LISTS CPACK_PACKAGE_FILES)
+  get_filename_component(PARENT_DIR "${PACKAGE_FILE}" DIRECTORY)
 
-execute_process(
-  COMMAND ar x "${CPACK_PACKAGE_FILES}"
-  WORKING_DIRECTORY "${TEMP_DIR}"
-  COMMAND_ERROR_IS_FATAL ANY)
+  set(TEMP_DIR "${PARENT_DIR}/tmp")
+  file(MAKE_DIRECTORY "${TEMP_DIR}")
 
-execute_process(
-  COMMAND tar -czvf "${CPACK_PACKAGE_FILES}" "."
-  WORKING_DIRECTORY "${TEMP_DIR}"
-  COMMAND_ERROR_IS_FATAL ANY)
+  execute_process(
+    COMMAND ar x "${PACKAGE_FILE}"
+    WORKING_DIRECTORY "${TEMP_DIR}"
+    COMMAND_ERROR_IS_FATAL ANY)
 
+  execute_process(
+    COMMAND tar -czf "${PACKAGE_FILE}" "."
+    WORKING_DIRECTORY "${TEMP_DIR}"
+    COMMAND_ERROR_IS_FATAL ANY)
+
+  message(STATUS "Created ${PACKAGE_FILE} as IPK")
+endforeach()
