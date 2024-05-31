023821e3fc350e537b5fcd97ce550e282b75772c
Timothy Werquin <timothy@werquin.com>
2024-05-31 21:10:19 +0200

    Tilem: download rom from ipfs

--- apps/tilem/Dialog.cpp
+++ apps/tilem/Dialog.cpp
@@ -6,8 +6,7 @@ void
 DownloadDialog::State::init(AppContext& ctx, const BuildContext& buildCtx) {
   startTimer = ctx.addTimer(std::chrono::seconds(0), [&] {
     constexpr auto url =
-      "http://web.archive.org/web/20240409191813id_/http://tiroms.weebly.com/"
-      "uploads/1/1/0/5/110560031/ti84plus.rom";
+      "http://ipfs.io/ipfs/QmSNmqjQ1Ao4jff9pXbzv98ebuCzKf2ZuyEpSGL31D8z44";
     auto cmd = "wget -O '" + getWidget().romPath + "' " + url;
 
     std::cout << cmd << "\n";
