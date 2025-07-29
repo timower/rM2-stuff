1b5bc8c4fea32cb151ba61871f68812212942fe2
Timothy Werquin <timothy@werquin.com>
2025-07-29 21:52:24 +0200

    Minor cmake configuration changes

--- cmake/BuildTypes.cmake
+++ cmake/BuildTypes.cmake
@@ -4,43 +4,40 @@ option(SANITIZE "Use asan in debug builds" OFF)
 # Set fPIC, even for shared libraries..
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
-if (NOT CMAKE_BUILD_TYPE)
+if(NOT CMAKE_BUILD_TYPE)
   set(CMAKE_BUILD_TYPE Debug)
 endif()
 
+# set(CMAKE_SbARED_LINKER_FLAGS "-static-libstdc++")
+#
+# set(CMAKE_EXE_LINKER_FLAGS "-static-libstdc++")
+
 set(BASE_OPTS
-  -fdiagnostics-color=always
-  -Wall
-  -Wextra
-  -Wno-unused-parameter
-  -Wno-gnu-statement-expression
-  -Wno-gnu-statement-expression-from-macro-expansion
-  -Wno-psabi)
+    -fdiagnostics-color=always
+    -Wall
+    -Wextra
+    -Wno-unused-parameter
+    -Wno-gnu-statement-expression
+    -Wno-gnu-statement-expression-from-macro-expansion
+    -Wno-psabi)
 
-set(COVER_OPTS
-  -fprofile-instr-generate
-  -fcoverage-mapping)
+set(COVER_OPTS -fprofile-instr-generate -fcoverage-mapping)
 
-if (COVERAGE)
+if(COVERAGE)
   list(APPEND BASE_OPTS ${COVER_OPTS})
 endif()
 
-set(ASAN_OPTS
-  -fsanitize=address
-  -fno-omit-frame-pointer
-  -fsanitize=undefined)
+set(ASAN_OPTS -fsanitize=address -fno-omit-frame-pointer -fsanitize=undefined)
 
-if (SANITIZE)
+if(SANITIZE)
   list(APPEND BASE_OPTS ${ASAN_OPTS} -Werror)
 endif()
 
 list(JOIN BASE_OPTS " " BASE_OPTS)
 
 set(CMAKE_C_FLAGS_DEBUG
-  "${BASE_OPTS} -g -Og"
-  CACHE STRING ""
-  FORCE)
+    "${BASE_OPTS} -g -Og"
+    CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS_DEBUG
-  "${BASE_OPTS} -Og -g -Wno-c++20-designator"
-  CACHE STRING ""
-  FORCE)
+    "${BASE_OPTS} -Og -g -Wno-c++20-designator"
+    CACHE STRING "" FORCE)
--- nix/rm2-toolchain.nix
+++ nix/rm2-toolchain.nix
@@ -13,7 +13,9 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://storage.googleapis.com/remarkable-codex-toolchain/3.20.0.92/rm2/remarkable-production-image-${version}-rm2-public-x86_64-toolchain.sh";
+    # url = "https://storage.googleapis.com/remarkable-codex-toolchain/3.15.4.2/remarkable-platform-image-4.1.112-rm2-public-x86_64-toolchain.sh";
     sha256 = "sha256-rqrAxV9yJTyGpAA2ElURTcMb+yNxyVrI9Yi3k3d7K6I=";
+    # sha256 = "sha256-dhnMnHhQ4cpDamqPSxN+Owc4009V6v+EzLrlZzbjt78=";
     executable = true;
   };
 
@@ -30,8 +32,13 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out
     ENVCLEANED=1 $src -y -d $out || true
-    rm -f $out/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/var/lock
+    rm -f $out/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/usr/lib/environment.d/99-environment.conf
+    rm -f $out/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/etc/tmpfiles.d/etc.conf
+    rm -f $out/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/etc/tmpfiles.d/home.conf
+    rm -f $out/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/etc/resolv.conf
     rm -f $out/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/etc/mtab
+    rm -f $out/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/etc/resolv-conf.systemd
+    rm -f $out/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/var/lock
   '';
 
   meta = with lib; {
