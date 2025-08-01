022e8ec82d6eac2bad96fe9bec9df278e17b9bb1
Timothy Werquin <timothy@werquin.com>
2025-08-01 23:44:31 +0200

    Format all CMake files

--- cmake/Deploy.cmake
+++ cmake/Deploy.cmake
@@ -1,47 +1,49 @@
-set(SSH_HOST "RemEmu" CACHE STRING "SSH host to deploy to")
+set(SSH_HOST
+    "RemEmu"
+    CACHE STRING "SSH host to deploy to")
 
 set(RUN_PREFIX
-  "LD_PRELOAD=/home/root/librm2fb_client.so"
-  CACHE STRING "Prefix when running over SSH")
+    "LD_PRELOAD=/home/root/librm2fb_client.so"
+    CACHE STRING "Prefix when running over SSH")
 
 find_program(SCP scp)
 find_program(SSH ssh)
 
-if ("${SCP}" STREQUAL "SCP-NOTFOUND")
+if("${SCP}" STREQUAL "SCP-NOTFOUND")
   message(WARNING "scp not found, skipping deploy")
 endif()
-if ("${SSH}" STREQUAL "SSH-NOTFOUND")
+if("${SSH}" STREQUAL "SSH-NOTFOUND")
   message(WARNING "scp not found, skipping deploy")
 endif()
 
 function(add_deploy target)
-  if (NOT TARGET ${target})
+  if(NOT TARGET ${target})
     message(FATAL_ERROR "add_deploy called on non-existent target: ${target}")
   endif()
 
-  if ("${SCP}" STREQUAL "SCP-NOTFOUND")
+  if("${SCP}" STREQUAL "SCP-NOTFOUND")
     return()
   endif()
 
-  add_custom_target(deploy_${target}
+  add_custom_target(
+    deploy_${target}
     COMMAND ${SCP} $<TARGET_FILE:${target}> "${SSH_HOST}:"
     DEPENDS ${target})
 
-  if ("${SSH}" STREQUAL "SSH-NOTFOUND")
+  if("${SSH}" STREQUAL "SSH-NOTFOUND")
     return()
   endif()
 
-  add_custom_target(run_${target}
-    COMMAND
-      ${SSH} -t -t "${SSH_HOST}"
-       ${RUN_PREFIX} "./$<TARGET_FILE_NAME:${target}>"
+  add_custom_target(
+    run_${target}
+    COMMAND ${SSH} -t -t "${SSH_HOST}" ${RUN_PREFIX}
+            "./$<TARGET_FILE_NAME:${target}>"
     DEPENDS deploy_${target}
     USES_TERMINAL)
 endfunction()
 
-
 function(add_opkg_targets)
-  if ("${SCP}" STREQUAL "SCP-NOTFOUND" OR "${SSH}" STREQUAL "SSH-NOTFOUND")
+  if("${SCP}" STREQUAL "SCP-NOTFOUND" OR "${SSH}" STREQUAL "SSH-NOTFOUND")
     return()
   endif()
 
@@ -49,9 +51,11 @@ function(add_opkg_targets)
   list(REMOVE_ITEM all_comps "Unspecified")
 
   foreach(comp IN LISTS all_comps)
-    add_custom_target(opkg_${comp}
+    add_custom_target(
+      opkg_${comp}
       COMMAND "${SCP}" "${CMAKE_BINARY_DIR}/${comp}.ipk" "${SSH_HOST}:"
-      COMMAND "${SSH}" "${SSH_HOST}" bash -l -c "'opkg install --force-reinstall ${comp}.ipk'"
+      COMMAND "${SSH}" "${SSH_HOST}" bash -l -c
+              "'opkg install --force-reinstall ${comp}.ipk'"
       DEPENDS package)
   endforeach()
 
--- cmake/GenIPK.cmake
+++ cmake/GenIPK.cmake
@@ -1,5 +1,5 @@
-# Cmake generates ar archives instead of tar.gz.
-# So this post archive script re-packages the .deb using tar.
+# Cmake generates ar archives instead of tar.gz. So this post archive script
+# re-packages the .deb using tar.
 
 foreach(PACKAGE_FILE IN LISTS CPACK_PACKAGE_FILES)
   get_filename_component(PARENT_DIR "${PACKAGE_FILE}" DIRECTORY)
@@ -7,15 +7,11 @@ foreach(PACKAGE_FILE IN LISTS CPACK_PACKAGE_FILES)
   set(TEMP_DIR "${PARENT_DIR}/tmp")
   file(MAKE_DIRECTORY "${TEMP_DIR}")
 
-  execute_process(
-    COMMAND ar x "${PACKAGE_FILE}"
-    WORKING_DIRECTORY "${TEMP_DIR}"
-    COMMAND_ERROR_IS_FATAL ANY)
+  execute_process(COMMAND ar x "${PACKAGE_FILE}"
+                  WORKING_DIRECTORY "${TEMP_DIR}" COMMAND_ERROR_IS_FATAL ANY)
 
-  execute_process(
-    COMMAND tar -czf "${PACKAGE_FILE}" "."
-    WORKING_DIRECTORY "${TEMP_DIR}"
-    COMMAND_ERROR_IS_FATAL ANY)
+  execute_process(COMMAND tar -czf "${PACKAGE_FILE}" "."
+                  WORKING_DIRECTORY "${TEMP_DIR}" COMMAND_ERROR_IS_FATAL ANY)
 
   message(STATUS "Created ${PACKAGE_FILE} as IPK")
 endforeach()
