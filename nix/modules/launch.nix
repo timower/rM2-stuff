aeb9b49f1ad0ad0b69620c51d16707331d447fcf
Timothy Werquin <timothy@werquin.com>
2025-11-13 21:08:01 +0100

    nixos: Add backup init for each system profile

--- nix/modules/launch.nix
+++ nix/modules/launch.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 let
   launcher = pkgs.callPackage ../pkgs/nixos-launcher.nix { };
 in
@@ -11,9 +11,23 @@ in
 
   # Use /sbin/init as a bootloader.
   # Copy nixctl, as /nix won't be bind mounted yet.
-  system.build.installBootLoader = pkgs.writeScript "install-sbin-init.sh" ''
-    #!${pkgs.runtimeShell}
-    ${pkgs.coreutils}/bin/ln -fs "$1/init" /sbin/init
-    ${pkgs.coreutils}/bin/cp "$1/nixctl" /nixctl
+  system.build.installBootLoader = pkgs.writeShellScript "install-sbin-init.sh" ''
+    export PATH="${
+      lib.makeBinPath [
+        pkgs.coreutils
+        pkgs.findutils
+      ]
+    }"
+
+    ln -fs "$1/init" /sbin/init
+    cp "$1/nixctl" /nixctl
+
+    for profile in /nix/var/nix/profiles/system-*-link; do
+      systemPath=$(readlink "$profile")
+      systemName=$(basename "$profile")
+      ln -fs "$systemPath/init" "/sbin/init-$systemName"
+    done
+
+    find /sbin/ -xtype l -delete
   '';
 }
